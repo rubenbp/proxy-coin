4f1d1e7f3843e6fc182ec5fa5d1b478d596de9e8
Ruben Bernardez Patier <ruben.bernardez@biko2.com>
2023-06-11 17:27:12 +0200

    feat: configura proxy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/proxy/:path*",
+        destination: "https://api.coingecko.com/:path*",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
